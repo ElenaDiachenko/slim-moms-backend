bd20d71ce1a1e9f6639f0b9bfb4ee0142403eec7
ElenaDiachenko <96205320+ElenaDiachenko@users.noreply.github.com>
2023-01-11 22:48:07 +0200

    fixed addOne

--- controllers/diary/addOne.js
+++ controllers/diary/addOne.js
@@ -1,33 +1,21 @@
 const { DailyProduct } = require('../../models');
 
-// const getProducts = async () => {
-//   const result = await BloodDietProduct.find({});
-//   return result;
-// };
-
 const addOne = async (req, res) => {
   const { _id } = req.user;
 
   const { weight, product, date, baseCaloricity } = req.body;
-  // const products = await getProducts();
-
-  // const productCaloricity = products.find(
-  //   it => it.title.ua === product
-  // ).calories;
 
   const calories = (baseCaloricity * weight) / 100;
 
-  const savedProduct = await DailyProduct.find({ date, owner: _id });
-
-  const duplicateProduct = await savedProduct.find(
-    it => it.product === product
-  );
-
+  const duplicateProduct = await DailyProduct.findOne({ product, date });
   let result;
 
   if (!duplicateProduct) {
     result = await DailyProduct.create({
-      ...req.body,
+      weight,
+      product,
+      date,
+      baseCaloricity,
       calories,
       owner: _id,
     });
@@ -46,17 +34,11 @@ const addOne = async (req, res) => {
     );
   }
 
-  !duplicateProduct
-    ? res.status(201).json({
-        status: 'succes',
-        code: 201,
-        result,
-      })
-    : res.status(200).json({
-        status: 'succes',
-        code: 200,
-        result,
-      });
+  res.status(!duplicateProduct ? 201 : 200).json({
+    status: 'success',
+    code: !duplicateProduct ? 201 : 200,
+    result,
+  });
 };
 
 module.exports = addOne;
